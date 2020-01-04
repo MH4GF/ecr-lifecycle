002f512b9a9a976602b39b6b2efb8a75d5347ebc
MH4GF <rixi436@gmail.com>
2020-01-04 16:38:40 +0900

    repositoriesを別ファイルに切り出す

---
+++ ecr/repositories.go
@@ -0,0 +1,25 @@
+package ecr
+
+import (
+	"github.com/aws/aws-sdk-go/service/ecr"
+)
+
+type repository struct {
+	original *ecr.Repository
+}
+
+func (c *client) DescribeRepositories() ([]*repository, error) {
+	input := &ecr.DescribeRepositoriesInput{}
+
+	result, err := c.ecr.DescribeRepositories(input)
+	if err != nil {
+		return nil, err
+	}
+
+	var repositories []*repository
+	for _, repo := range result.Repositories {
+		repositories = append(repositories, &repository{original: repo})
+	}
+
+	return repositories, nil
+}
