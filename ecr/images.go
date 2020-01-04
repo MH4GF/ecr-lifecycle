82a4c207cdd29e58f5435e0bd8b5694fbe554e46
MH4GF <rixi436@gmail.com>
2020-01-04 16:39:42 +0900

    実行中のタスクがある場合、イメージを保護

--- ecr/images.go
+++ ecr/images.go
@@ -2,68 +2,30 @@ package ecr
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
+	"github.com/Taimee/ecr-lifecycle/ecs"
 	"github.com/aws/aws-sdk-go/service/ecr"
 )
 
-func (c *client) BatchDeleteImages(repositoryName string) error {
-	imageIds, err := c.deleteTargetImageIds(repositoryName, 50)
+type Image struct {
+	original *ecr.Image
+}
+
+// Image型にはURIがないので定義
+// 012345678910.dkr.ecr.<region-name>.amazonaws.com/<repository-name>:latest
+func (i *Image) uri(r *repository) *string {
+	uri := *r.original.RepositoryUri + ":" + *i.original.ImageId.ImageTag
+	return &uri
+}
+
+func (c *client) BatchDeleteImages(r *repository, imageCountMoreThan *int) error {
+	input, err := c.NewRegisterBatchDeleteImageInput(r, imageCountMoreThan)
 	if err != nil {
 		return err
 	}
 
 	//imageの存在判定
-	if len(imageIds) == 0 {
+	if len(input.ImageIds) == 0 {
 		return nil
-	}
-
-	//全てを消す
-	err = c.batchDeleteImage(imageIds, repositoryName)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (c *client) describeImages(repositoryName string) ([]*ecr.ImageDetail, error) {
-	input := &ecr.DescribeImagesInput{
-		RepositoryName: &repositoryName,
-	}
-
-	result, err := c.ecr.DescribeImages(input)
-	if err != nil {
-		return nil, err
-	}
-
-	return result.ImageDetails, nil
-}
-
-func (c *client) deleteTargetImageIds(repositoryName string, imageCountMoreThan int) ([]*ecr.ImageIdentifier, error) {
-	images, err := c.describeImages(*aws.String(repositoryName))
-	if err != nil {
-		return nil, err
-	}
-
-	var imageIds []*ecr.ImageIdentifier
-
-	for i, image := range images {
-		if i <= imageCountMoreThan {
-			continue
-		}
-
-		imageIds = append(imageIds, &ecr.ImageIdentifier{
-			ImageDigest: image.ImageDigest,
-		})
-	}
-
-	return imageIds, nil
-}
-
-func (c *client) batchDeleteImage(imageIds []*ecr.ImageIdentifier, repositoryName string) error {
-	input := &ecr.BatchDeleteImageInput{
-		ImageIds: imageIds,
-		RepositoryName: &repositoryName,
 	}
 
 	result, err := c.ecr.BatchDeleteImage(input)
@@ -73,4 +35,93 @@ func (c *client) batchDeleteImage(imageIds []*ecr.ImageIdentifier, repositoryNam
 
 	fmt.Println(result)
 	return nil
+}
+
+func (c *client) NewRegisterBatchDeleteImageInput(r *repository, imageCountMoreThan *int) (*ecr.BatchDeleteImageInput, error) {
+	images, err := c.BatchGetImages(r)
+	if err != nil {
+		return nil, err
+	}
+
+	ecsClient, err := ecs.NewClient("sandbox", "ap-northeast-1")
+	if err != nil {
+		return nil, err
+	}
+	runningTasks, err := ecsClient.ListAllRunningTasks()
+	if err != nil {
+		return nil, err
+	}
+
+	var imageIds []*ecr.ImageIdentifier
+
+	for i, image := range images {
+		if i < *imageCountMoreThan {
+			continue
+		}
+
+		// 現在実行中のタスクがある場合は削除しない
+		if image.isUsedRunningTasks(runningTasks, r) {
+			continue
+		} else {
+			imageIds = append(imageIds, image.original.ImageId)
+		}
+	}
+
+	input := &ecr.BatchDeleteImageInput{
+		ImageIds:       imageIds,
+		RepositoryName: r.original.RepositoryName,
+	}
+
+	return input, nil
+}
+
+func (i *Image) isUsedRunningTasks(tasks []*ecs.Task, r *repository) bool {
+	uri := i.uri(r)
+
+	for _, task := range tasks {
+		if *task.Image == *uri {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (c *client) BatchGetImages(r *repository) ([]*Image, error) {
+	input, err := c.newRegisterBatchGetImageInput(r)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := c.ecr.BatchGetImage(input)
+	if err != nil {
+		return nil, err
+	}
+
+	var images []*Image
+	for _, image := range result.Images {
+		images = append(images, &Image{original: image})
+	}
+
+	return images, nil
+}
+
+func (c *client) newRegisterBatchGetImageInput(r *repository) (*ecr.BatchGetImageInput, error) {
+	input := &ecr.DescribeImagesInput{
+		RepositoryName: r.original.RepositoryName,
+	}
+
+	result, err := c.ecr.DescribeImages(input)
+	if err != nil {
+		return nil, err
+	}
+
+	var imageIds []*ecr.ImageIdentifier
+	for _, imageDetail := range result.ImageDetails {
+		imageIds = append(imageIds, &ecr.ImageIdentifier{ImageDigest: imageDetail.ImageDigest})
+	}
+
+	batchInput := &ecr.BatchGetImageInput{ImageIds: imageIds, RepositoryName: r.original.RepositoryName}
+
+	return batchInput, nil
 }
