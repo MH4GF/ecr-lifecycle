d27d153a23c4e300b1d7d0159aac82708974bf01
MH4GF <rixi436@gmail.com>
2020-01-13 16:21:24 +0900

    IsImageUsedRunningTasksのテストを追加

--- ecr/image_test.go
+++ ecr/image_test.go
@@ -1,6 +1,7 @@
 package ecr
 
 import (
+	"github.com/Taimee/ecr-lifecycle/ecs"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ecr"
 	"reflect"
@@ -49,7 +50,7 @@ func TestImage_Uris(t *testing.T) {
 		{
 			input: Image{
 				Detail: &ecr.ImageDetail{
-					ImageTags: []*string{},
+					ImageTags:   []*string{},
 					ImageDigest: aws.String("sha256:example"),
 				},
 			},
@@ -62,6 +63,63 @@ func TestImage_Uris(t *testing.T) {
 	for _, c := range cases {
 		if !reflect.DeepEqual(c.input.Uris(r), c.expected) {
 			t.Errorf("Expected to contain %v, but not.", c.expected)
+		}
+	}
+}
+
+func TestImage_IsImageUsedRunningTasks(t *testing.T) {
+	r := ecr.Repository{
+		RepositoryUri: aws.String("012345678910.dkr.ecr.ap-northeast-1.amazonaws.com/hoge"),
+	}
+	i := Image{
+		Detail: &ecr.ImageDetail{
+			ImageTags: []*string{
+				aws.String("latest"),
+			},
+			ImageDigest: aws.String("sha256:example"),
+		},
+	}
+
+	cases := []struct {
+		input    []ecs.Task
+		expected bool
+	}{
+		{
+			input:    []ecs.Task{},
+			expected: false,
+		},
+		{
+			input: []ecs.Task{
+				{
+					Image: "012345678910.dkr.ecr.ap-northeast-1.amazonaws.com/hoge:latest",
+				},
+			},
+			expected: true,
+		},
+		{
+			input: []ecs.Task{
+				{
+					Image: "012345678910.dkr.ecr.ap-northeast-1.amazonaws.com/hoge:different-tag",
+				},
+			},
+			expected: false,
+		},
+		{
+			input: []ecs.Task{
+				{
+					Image: "012345678910.dkr.ecr.ap-northeast-1.amazonaws.com/hoge:latest",
+				},
+				{
+					Image: "012345678910.dkr.ecr.ap-northeast-1.amazonaws.com/hoge:different-tag",
+				},
+			},
+			expected: true,
+		},
+	}
+
+	for _, c := range cases {
+		if i.IsImageUsedRunningTasks(c.input, r) != c.expected {
+			t.Errorf("Expected value to be %v, but not.", c.expected)
 		}
 	}
 }
